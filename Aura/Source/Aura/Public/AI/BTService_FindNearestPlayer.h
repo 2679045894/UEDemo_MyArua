5cdbc86b72d7ddc9e9a2585f0d7e884fa6a9a139
2679045894 <2679045894@qq.com>
2025-11-09 09:38:56 +0800

    64.BehaviorService

---
+++ Aura/Source/Aura/Public/AI/BTService_FindNearestPlayer.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/Services/BTService_BlueprintBase.h"
+#include "BTService_FindNearestPlayer.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API UBTService_FindNearestPlayer : public UBTService_BlueprintBase
+{
+	GENERATED_BODY()
+protected:
+	//TickNode 的主要作用是让任务能够在多个帧中持续运行，并在每一帧执行特定的逻辑检查或渐进式操作。
+	virtual void TickNode(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds) override;
+};
